60467d2808c4bf4c583bed2108292886f5275fa0
SantiagoRF <106044732+Soverd25@users.noreply.github.com>
2023-10-09 16:53:19 -0500

    Add files via upload
    Terminación del Html y css

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// variables de el aviso de cookies
+const box = document.querySelector('.box')  
+const boton = document.querySelector('.boton') 
+// fin de las variables de cookies  
+
+setTimeout(()=>{
+    box.style.marginBottom = "0" 
+    box.style.transition = "1.25s" 
+    box.style.transition = "1s"
+},3000) 
+
+
+//listeners 
+boton.addEventListener('click', ()=>{
+    box.style.marginBottom = "-500px"
+    box.style.transition = "1.25s" 
+})
