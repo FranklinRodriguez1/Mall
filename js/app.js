a302261fd5b1b5199851d37b14c990bf1c0a23ce
Franklin Rodriguez <frro2121.m@gmail.com>
2023-10-24 23:42:23 -0500

    se mejora los estilos de css para dar mejor visual de los elementos se genera alertas con js al no llenar un valor en un input se debe terminar de trabajar en el email la validacion del mismo

--- js/app.js
+++ js/app.js
@@ -6,15 +6,14 @@ const boton = document.querySelector('.boton')
     const formulario = document.getElementById("formulario")
     const nombre = document.querySelector(".nombre")
     const email = document.querySelector(".email") 
-    const telefono = document.querySelector(".telefono") 
+    const telefono = document.querySelector("#phone") 
     const mensaje  = document.querySelector(".mensaje") 
     const enviar = document.querySelector(".enviar") 
-    const limpiar = document.querySelector(".limpiar")
+    const limpiar = document.querySelector(".limpiar") 
+    const alertas = document.querySelector(".alertas")
 //fin del formulario 
 
-/*codigo para el formulario*/ 
 
-/* fin del formulario*/
 /*  codigo para las cookies*/ 
 setTimeout(()=>{
     box.style.marginBottom = "0" 
@@ -30,4 +29,38 @@ boton.addEventListener('click', ()=>{
 // listener para el formulario 
 limpiar.addEventListener('click', (e)=>{ 
     e.preventDefault()
-    formulario.reset()})
+    formulario.reset()}) 
+
+    nombre.addEventListener('blur',validar)
+    email.addEventListener('blur', validar)
+    telefono.addEventListener('blur',validar)
+    mensaje.addEventListener ('blur',validar)
+// fin de los listeners del formularios 
+
+//clases 
+
+//funciones para el formulario
+function validar(e){   
+    if(e.target.value.trim() === ""){
+        let alertaError = document.querySelector("p"); 
+        alertaError.classList.add(
+            "text-light",
+            "bg-success",
+            "fw-bolder")
+        alertaError.textContent = `el campo ${e.target.classList} es obligatorio`; 
+        alertas.appendChild(alertaError) 
+        setTimeout(()=>{
+            alertaError.remove()
+        },3000)
+    } 
+    if(e.target.classList.contain("email") && e.target.value !== ""){
+        validarEmail(e.target.value); 
+        return
+    }
+}  
+    function validarEmail(email){ 
+    const exprecionReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(exprecionReg.test(email)){ 
+        console.log("correo electronico valido");
+    }else{console.log("correo electronico no valido");}
+}
