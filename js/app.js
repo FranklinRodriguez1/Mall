a29f92ad8b0511333563ffb9769ce02028834fe1
Franklin Rodriguez <frro2121.m@gmail.com>
2023-10-26 01:28:21 -0500

    se da funcionalidad casi completa al formulario queda faltando las tansiciones y darle funcionalidad a los botones del menu

--- js/app.js
+++ js/app.js
@@ -10,7 +10,9 @@ const boton = document.querySelector('.boton')
     const mensaje  = document.querySelector(".mensaje") 
     const enviar = document.querySelector(".enviar") 
     const limpiar = document.querySelector(".limpiar") 
-    const alertas = document.querySelector(".alertas")
+    const alertas = document.querySelector(".alertas") 
+    let estado = false 
+    
 //fin del formulario 
 
 
@@ -29,38 +31,96 @@ boton.addEventListener('click', ()=>{
 // listener para el formulario 
 limpiar.addEventListener('click', (e)=>{ 
     e.preventDefault()
-    formulario.reset()}) 
-
-    nombre.addEventListener('blur',validar)
-    email.addEventListener('blur', validar)
-    telefono.addEventListener('blur',validar)
-    mensaje.addEventListener ('blur',validar)
-// fin de los listeners del formularios 
-
-//clases 
-
-//funciones para el formulario
-function validar(e){   
-    if(e.target.value.trim() === ""){
-        let alertaError = document.querySelector("p"); 
-        alertaError.classList.add(
+    formulario.reset() 
+    let alertaError = document.createElement("p"); 
+        alertaError.classList.add( 
+            "aviso",
             "text-light",
             "bg-success",
-            "fw-bolder")
-        alertaError.textContent = `el campo ${e.target.classList} es obligatorio`; 
+            "fw-bolder",
+            "pt-1")
+        alertaError.textContent = `Se ha limpiado el formulario`; 
         alertas.appendChild(alertaError) 
         setTimeout(()=>{
             alertaError.remove()
         },3000)
+} 
+    ) 
+
+    nombre.addEventListener('blur',validar)
+    email.addEventListener('blur', validar)
+    telefono.addEventListener('blur',validar)
+    mensaje.addEventListener ('blur',validar) 
+    enviar.addEventListener('click', (e)=>{ 
+        e.preventDefault()
+        let nuevaPersona  = new persona(nombre.value, email.value, telefono.value, mensaje.value);  
+        let alertaError = document.createElement("p"); 
+        alertaError.classList.add( 
+            "aviso",
+            "text-light",
+            "bg-success",
+            "fw-bolder",
+            "pt-1")
+        alertaError.textContent = `Se ha hecho el envio correctamente`; 
+        alertas.appendChild(alertaError) 
+        setTimeout(()=>{
+            alertaError.remove() 
+        },3000) 
+        formulario.reset()     
+        console.log(nuevaPersona); 
+        enviar.disabled = true
+    })
+// fin de los listeners del formularios 
+//clase
+class persona {
+    constructor(nombre, correo, numeroTelf, mensaje){
+        this.nombre  = nombre
+        this.correo = correo
+        this.numeroTelf = numeroTelf
+        this.mensaje = mensaje
+    }
+}
+//funciones para el formulario
+function validar(e){   
+    if(e.target.value.trim() === ""){
+        let alertaError = document.createElement("p"); 
+        alertaError.classList.add( 
+            "aviso",
+            "text-light",
+            "bg-danger",
+            "fw-bolder",
+            "pt-1")
+        alertaError.textContent = `el campo ${e.target.classList} es obligatorio`; 
+        alertas.appendChild(alertaError) 
+        setTimeout(()=>{
+            alertaError.remove() 
+            alertaError
+        },3000)
     } 
-    if(e.target.classList.contain("email") && e.target.value !== ""){
-        validarEmail(e.target.value); 
-        return
+    if(e.target.classList.contains("email") && e.target.value != ""){
+        validarEmail(e.target.value.trim()); 
+        
+    }if(estado === true && nombre != "" && telefono != "" && mensaje != "" ){
+        enviar.disabled = false
     }
 }  
     function validarEmail(email){ 
     const exprecionReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(exprecionReg.test(email)){ 
-        console.log("correo electronico valido");
-    }else{console.log("correo electronico no valido");}
+    if(exprecionReg.test(email)){  
+        estado = true
+        return
+    }else{
+        let alertaError = document.createElement("p"); 
+        alertaError.classList.add( 
+            "aviso",
+            "text-light",
+            "bg-danger",
+            "fw-bolder",
+            "pt-1")
+        alertaError.textContent = `Correo electronico no es válido`; 
+        alertas.appendChild(alertaError) 
+        setTimeout(()=>{
+            alertaError.remove()
+        },3000)
+    }
 }
